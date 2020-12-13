3af21068927049714d66ecbe40ba6c77f1537020
MuratG <muratgozderesi34@gmail.com>
2020-12-13 13:59:20 +0300

    added book model

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
-
 const router = express.Router();
 const AuthRouter = require("./AuthRouter");
-// const ProfileRouter = require("./ProfileRouter");
-// const BookRouter = require("./BookRouter");
+const ProfileRouter = require("./ProfileRouter");
+const BookRouter = require("./BookRouter");
 
 // Only /api endpoint
 
@@ -18,12 +17,12 @@ router.use("/auth", AuthRouter);
  * @route /api/profile
  * @desc Route for Profile
  */
-//router.use("/profile", ProfileRouter);
+router.use("/profile", ProfileRouter);
 
 /**
  * @route /api/book
  * @desc Route for Book
  */
-//router.use("/book", BookRouter);
+router.use("/book", BookRouter);
 
 module.exports = router;
